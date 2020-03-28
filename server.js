7c20b0feda7379e29fc727a7acd861c503b51884
gideonrynn <sarah.c.dillard@gmail.com>
2020-03-28 14:11:08 -0500

    initial files

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const app = express();
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+// API routes here
 
 // Send every other request to the React app
 // Define any API routes before this runs
