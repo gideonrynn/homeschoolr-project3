20ff457c53b43cbc5afc2013b62320f8603979d3
gideonrynn <sarah.c.dillard@gmail.com>
2020-04-02 18:16:08 -0500

    export user router

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,3 +2,5 @@ const db = require("../../models");
 const passport = require("../../config/passport");
 const router = require("express").Router();
 const userController = require("../../controllers/userauthController");
+
+module.exports = router; 
