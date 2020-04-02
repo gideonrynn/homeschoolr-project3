6e8b2a93228cce8c240291e18b332dc817b862d0
lmalexander <elizabeth.alexander@northwestern.edu>
2020-04-02 18:04:23 -0500

    rough written components: selectSubj and selectTime dropdown menus, schedule input form, and scheduler

---
+++ client/src/pages/Parent.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import MuiThemeProvider from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+
+class Parent extends Component {
+    
+    state = {}
+
+    componentWillMount() {}
+
+    render() {
+        return ()
+    }
+}
