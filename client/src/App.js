72f4c69a65f0bce1e08bda7aed9ed0c3b251f8a1
unknown <yhlee89.dho@gmail.com>
2020-04-06 02:37:36 -0500

    changed form css

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
   render () {
     return (
       <Router>
-        <div>
+        <div className="mainPage">
           <NavBar />
           {/* <Header /> */}
           <Wrapper>
--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -1,24 +1,47 @@
 import React, { Component } from 'react';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+// import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar';
+import VpnKeyOutlinedIcon from '@material-ui/icons/VpnKeyOutlined';
+
 import Login from '../Login';
-import AuthAPI from "../../utils/userAuthAPI"
+import AuthAPI from "../../utils/userAuthAPI";
 
-const theme = createMuiTheme();
-    theme.typography.h3 = {
-        fontSize: '1.2rem',
-        '@media (min-width:600px)': {
-        fontSize: '1.5rem',
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
     },
-    [theme.breakpoints.up('md')]: {
-        fontSize: '2.4rem',
-    }
-};
+    form: {
+      width: '100%',
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+}));
+// const theme = createMuiTheme();
+//     theme.typography.h3 = {
+//         fontSize: '1.2rem',
+//         '@media (min-width:600px)': {
+//         fontSize: '1.5rem',
+//     },
+//     [theme.breakpoints.up('md')]: {
+//         fontSize: '2.4rem',
+//     }
+// };
 
-const margin = {
-    margin: 15,
-};
+// const margin = {
+//     margin: 15,
+// };
 
 class Register extends Component {
 
@@ -84,13 +107,25 @@ class Register extends Component {
 
     render() {
         return (
-            <div>
-                <MuiThemeProvider>
-                    <div>
-                        {/* <Typography>Register</Typography> */}
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <div className={useStyles.paper}>
+                    <Avatar className={useStyles.avatar}>
+                        <VpnKeyOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Register
+                    </Typography>
 
+                    <form className={useStyles.form} noValidate>
                         <TextField
-                            helperText="Enter your Child's Name"
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="studentName"
+                            label="Student's Name"
+                            autoFocus
                             // floatingLabelText="Child Name"
 
                             //this did not work for scd
@@ -102,7 +137,13 @@ class Register extends Component {
                         <br/>
 
                         <TextField
-                            helperText="Enter your Name"
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="parentName"
+                            label="Parent's Name"
+                            autoFocus
                             // floatingLabelText="Parent Name"
 
                             //this did not work for scd
@@ -114,8 +155,14 @@ class Register extends Component {
                         <br/>
 
                         <TextField
-                            helperText="Enter your Email"
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
                             type="email"
+                            autoFocus
                             // floatingLabelText="Email"
 
                             //this did not work for scd
@@ -128,8 +175,14 @@ class Register extends Component {
                         <br/>
 
                         <TextField
-                            type = "password"
-                            helperText="Enter your Password"
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="password"
+                            label="Password"
+                            type="password"
+                            autoFocus
                             // floatingLabelText="Password"
 
                             //this did not work for scd
@@ -140,11 +193,13 @@ class Register extends Component {
 
                         <br/>
 
-                        <Button label="Submit" primary={true} style={margin} onClick={(event) => this.handleClick(event)}>Submit</Button>
+                        <Button label="Submit" primary={true} onClick={(event) => this.handleClick(event)}>Submit</Button>
 
-                    </div>
-                </MuiThemeProvider>
-            </div>
+
+                    </form>
+                    
+                </div>
+            </Container>
         );
     }
 }
